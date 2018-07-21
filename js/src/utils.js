c76bdc5f0ebf3589cd976de77022d04598622b59
NiShuang <klqbtnsns@hotmail.com>
2018-07-21 17:58:37 +0800

    Site updated: 2018-07-21 17:58:37

--- js/src/utils.js
+++ js/src/utils.js
@@ -300,7 +300,7 @@ $(document).ready(function() {
       updateSidebarHeightTimer = setTimeout(function() {
         var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
 
-        updateSidebarHeight(sidebarWrapperHeight);
+        updateSidebarHeight(sidebarWrapperHeight + 900 -638);
       }, 0);
     });
 
@@ -314,7 +314,7 @@ $(document).ready(function() {
     }
 
     // Initialize Sidebar & TOC Height.
-    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
+    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding() + 900 -638);
   }
 
   initSidebarDimension();
